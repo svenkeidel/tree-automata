c9a9c7206e0c43913bbd91a411daefa4aa74d52b
Jente Hidskes <hjdskes@gmail.com>
2018-09-03 11:15:33 +0200

    Add topological clash widening operators
    This is a widening operator by Hentenryck et al. that is guided by
    differences in the shapes of the grammars of successive steps.  See
    their paper "Type analysis of prolog using type graphs".

--- test/TreeAutomataSpec.hs
+++ test/TreeAutomataSpec.hs
@@ -5,6 +5,7 @@ import           Control.Monad.State hiding (sequence)
 
 import           Data.List hiding (union)
 import qualified Data.Map as M
+import qualified Data.Set as S
 import           Data.Text (Text)
 
 import           TreeAutomata
@@ -283,6 +284,192 @@ spec = do
       det `shouldSatisfy` isDeterministic
       det `shouldBe` infinite
 
+  describe "Widening" $ do
+    it "wideMap should compute the depths and principal label sets of nonterminals in PCF" $ do
+      let pcf' = evalState pcf 0
+          pcf_wideMap = wideMap pcf'
+          pcf_prods = productions pcf'
+      M.lookup "PStart" pcf_wideMap `shouldBe` Just (0,S.empty,pcf_prods M.! "PStart")
+      M.lookup "Exp" pcf_wideMap `shouldBe` Just (1,S.fromList ["App","Abs","Zero","Succ","Pred","Ifz"],pcf_prods M.! "Exp")
+      M.lookup "String" pcf_wideMap `shouldBe` Just (2,S.singleton "String",pcf_prods M.! "String")
+      M.lookup "Type" pcf_wideMap `shouldBe` Just (1,S.fromList ["Num","Fun"],pcf_prods M.! "Type")
+
+    it "wideMap should compute the depths and principal label sets of nonterminals in the nondeterministic grammar" $ do
+      let nondet'' = evalState nondet' 0
+          nondet_wideMap = wideMap nondet''
+          nondet_prods = productions nondet''
+      M.lookup "S" nondet_wideMap `shouldBe` Just (0,S.empty,nondet_prods M.! "S")
+      M.lookup "F" nondet_wideMap `shouldBe` Just (1,S.singleton "f",nondet_prods M.! "F")
+      M.lookup "G" nondet_wideMap `shouldBe` Just (2,S.singleton "g",nondet_prods M.! "G")
+      M.lookup "H" nondet_wideMap `shouldBe` Just (2,S.empty,nondet_prods M.! "H")
+      M.lookup "A" nondet_wideMap `shouldBe` Just (3,S.singleton "a",nondet_prods M.! "A")
+
+    it "should build a correspondence set on the example from the paper" $ do
+      let cons0' = evalState (epsilonClosure cons0) 0
+          cons1' = evalState (epsilonClosure cons1) 0
+          cons_w0 = wideMap cons0'
+          cons_w1 = wideMap cons1'
+      correspondenceSet (start cons0') cons_w0 (start cons1') cons_w1 `shouldBe` M.fromList [(("T0","T3"),S.empty)
+                                                                                            ,(("T1","T4"),S.singleton ("cons","T3",0))
+                                                                                            ,(("T2","T5"),S.singleton ("cons","T3",1))]
+
+    it "should build a correspondence set on the PCF grammar " $ do
+      let pcf_sub' = evalState (epsilonClosure pcf_sub) 0
+          pcf' = evalState (epsilonClosure pcf) 0
+          pcf_sub_w = wideMap pcf_sub'
+          pcf_w = wideMap pcf'
+      correspondenceSet (start pcf_sub') pcf_sub_w (start pcf') pcf_w `shouldBe` M.fromList [(("PSStart","PStart"),S.empty)]
+
+    it "should build a correspondence set on the arithmetic example from the paper" $ do
+      let arith0' = evalState (epsilonClosure arith0) 0
+          arith1' = evalState (epsilonClosure arith1) 0
+          arith_w0 = wideMap arith0'
+          arith_w1 = wideMap arith1'
+      correspondenceSet (start arith0') arith_w0 (start arith1') arith_w1 `shouldBe` M.fromList [(("T0","Tn"),S.empty)
+                                                                                                ,(("Tx","T3"),S.fromList [("Add","Tn",0)
+                                                                                                                         ,("par","T7",0)])
+                                                                                                ,(("T1","T6"),S.fromList [("Add","Tn",1)
+                                                                                                                         ,("Mul","T6",0)])
+                                                                                                ,(("T2","T7"),S.singleton ("Mul","T6",1))]
+
+    it "should find a set of widening topological clashes on the example from the paper" $ do
+      let cons0' = evalState (epsilonClosure cons0) 0
+          cons1' = evalState (epsilonClosure cons1) 0
+          cons_w0 = wideMap cons0'
+          cons_w1 = wideMap cons1'
+          corr = correspondenceSet (start cons0') cons_w0 (start cons1') cons_w1
+      wideningClashes corr cons_w0 cons_w1 `shouldBe` M.fromList [(("T2","T5"),S.singleton ("cons","T3",1))]
+
+    it "should find a set of widening topological clashes on the PCF grammar" $ do
+      let pcf_sub' = evalState (epsilonClosure pcf_sub) 0
+          pcf' = evalState (epsilonClosure pcf) 0
+          pcf_sub_w = wideMap pcf_sub'
+          pcf_w = wideMap pcf'
+          corr = correspondenceSet (start pcf_sub') pcf_sub_w (start pcf') pcf_w
+      wideningClashes corr pcf_sub_w pcf_w `shouldBe` M.fromList [(("PSStart","PStart"),S.empty)]
+
+    it "should find a set of widening topological clashes on the arithmetic example from the paper" $ do
+      let arith0' = evalState (epsilonClosure arith0) 0
+          arith1' = evalState (epsilonClosure arith1) 0
+          arith_w0 = wideMap arith0'
+          arith_w1 = wideMap arith1'
+          corr = correspondenceSet (start arith0') arith_w0 (start arith1') arith_w1
+      wideningClashes corr arith_w0 arith_w1 `shouldBe` M.fromList [(("Tx","T3"),S.fromList [("Add","Tn",0)
+                                                                                            ,("par","T7",0)])]
+
+    it "should find ancestors for the example from the paper" $ do
+      let cons1' = evalState cons1 0
+          w = wideMap cons1'
+      findAncestors "T5" w cons1' `shouldBe` ["T3"]
+
+    it "should find ancestors for the arithmetic example from the paper" $ do
+      let arith1' = evalState arith1 0
+          w = wideMap arith1'
+      findAncestors "T3" w arith1' `shouldBe` ["T6","T7","Tn"]
+
+    it "should find ancestors for PCF" $ do
+      let pcf' = evalState pcf 0
+          w = wideMap pcf'
+      findAncestors "PStart" w pcf' `shouldBe` []
+      findAncestors "Exp" w pcf' `shouldBe` ["PStart"]
+      findAncestors "String" w pcf' `shouldBe` ["Exp","PStart"]
+      findAncestors "Type" w pcf' `shouldBe` ["Exp","PStart"]
+
+    it "should find ancestors for the nondeterministic grammar" $ do
+      let nondet'' = evalState nondet' 0
+          w = wideMap nondet''
+      findAncestors "G" w nondet'' `shouldBe` ["F", "H", "S"]
+      findAncestors "A" w nondet'' `shouldBe` ["F", "G", "H", "S"]
+
+    it "should find the best arc ancestor for the example from the paper" $ do
+      let cons0' = evalState (epsilonClosure cons0) 0
+          cons1' = evalState (epsilonClosure cons1) 0
+          w0 = wideMap cons0'
+          w1 = wideMap cons1'
+          ancs = findAncestors "T5" w1 cons1'
+      arcAncestor "T2" "T5" ancs w0 w1 cons1' `shouldBe` Just "T3"
+
+    it "should find the best arc ancestor for the arithmetic example from the paper" $ do
+      let arith0' = evalState (epsilonClosure arith0) 0
+          arith1' = evalState (epsilonClosure arith1) 0
+          w0 = wideMap arith0'
+          w1 = wideMap arith1'
+          ancs = findAncestors "T3" w1 arith1'
+      arcAncestor "Tx" "T3" ancs w0 w1 arith1' `shouldBe` Just "Tn"
+
+    it "should find the best arc ancestor for PCF" $ do
+      let pcf' = evalState (epsilonClosure pcf) 0
+          w1 = wideMap pcf'
+      arcAncestor "PSStart" "PStart" (findAncestors "PStart" w1 pcf') w1 w1 pcf' `shouldBe` Nothing
+      arcAncestor "Exp" "Exp" (findAncestors "Exp" w1 pcf') w1 w1 pcf' `shouldBe` Just "PStart"
+      arcAncestor "String" "String" (findAncestors "String" w1 pcf') w1 w1 pcf' `shouldBe` Nothing
+      arcAncestor "Type" "Type" (findAncestors "Type" w1 pcf') w1 w1 pcf' `shouldBe` Just "PStart"
+
+    it "should find a set of arc replacements for the widening topological clashes for the example from the paper" $ do
+      let cons0' = evalState (epsilonClosure cons0) 0
+          cons1' = evalState (epsilonClosure cons1) 0
+          w0 = wideMap cons0'
+          w1 = wideMap cons1'
+          corr = correspondenceSet (start cons0') w0 (start cons1') w1
+          wideClashes01 = wideningClashes corr w0 w1
+      arcReplacements cons1' wideClashes01 w0 w1 `shouldBe` M.fromList [(("T5","T3"),S.singleton ("cons","T3",1))]
+
+    it "should find a set of arc replacements for the widening topological clashes for the PCF grammar" $ do
+      let pcf_sub' = evalState (epsilonClosure pcf_sub) 0
+          pcf' = evalState (epsilonClosure pcf) 0
+          w0 = wideMap pcf_sub'
+          w1 = wideMap pcf'
+          corr = correspondenceSet (start pcf_sub') w0 (start pcf') w1
+          wideClashes = wideningClashes corr w0 w1
+      arcReplacements pcf' wideClashes w0 w1 `shouldBe` M.fromList []
+
+    it "should find a set of arc replacements for the widening topological clashes for the arithmetic example from the paper " $ do
+      let arith0' = evalState (epsilonClosure arith0) 0
+          arith1' = evalState (epsilonClosure arith1) 0
+          w0 = wideMap arith0'
+          w1 = wideMap arith1'
+          corr = correspondenceSet (start arith0') w0 (start arith1') w1
+          wideClashes = wideningClashes corr w0 w1
+      arcReplacements arith1' wideClashes w0 w1 `shouldBe` M.fromList [(("T3","Tn"),S.fromList [("Add","Tn",0)
+                                                                                               ,("par","T7",0)])]
+
+    it "should replace nonterminals with ancestors" $ do
+      let consr :: GrammarBuilder Text
+          consr = grammar "T3" $ M.fromList [ ("T3", [ Ctor "nil" [], Ctor "cons" ["T4","T3"] ])
+                                            , ("T4", [ Ctor "any" [] ])
+                                            , ("T6", [ Ctor "any" [] ])
+                                            , ("T7", [ Ctor "nil" [] ])]
+          cons1' = evalState (epsilonClosure cons1) 0
+      return (replaceNonterm "T5" "T3" cons1') `shouldBeLiteral` consr
+
+    it "should replace an edge" $ do
+      let consr :: GrammarBuilder Text
+          consr = grammar "T3" $ M.fromList [ ("T3", [ Ctor "nil" [], Ctor "cons" ["T4","T3"] ])
+                                            , ("T4", [ Ctor "any" [] ])
+                                            , ("T5", [ Ctor "nil" [], Ctor "cons" ["T6","T7"] ])
+                                            , ("T6", [ Ctor "any" [] ])
+                                            , ("T7", [ Ctor "nil" [] ])]
+          cons1' = evalState (epsilonClosure cons1) 0
+      return (replaceEdge "cons" "T3" 1 "T3" cons1') `shouldBeLiteral` consr
+
+    it "should work on the examples from the paper" $ do
+      widen' cons0 cons1 `shouldBeLiteral` cons01
+      widen' cons1 cons2 `shouldBeLiteral` cons12
+      widen' arith0 arith1 `shouldBe` arith01
+      widen' arith1 (widen' arith0 arith1) `shouldBeLiteral` arith01
+      widen' arith2 arith3 `shouldBeLiteral` arith3
+
+    it "should be an upper bound" $ do
+      let w_cons = widen' cons0 cons1
+          w_pcf = widen' pcf_sub pcf
+          w_arith = widen' arith0 arith1
+      pcf `shouldSatisfy` (`subsetOf` w_pcf)
+      pcf_sub `shouldSatisfy` (`subsetOf` w_pcf)
+      cons0 `shouldSatisfy` subsetOf w_cons
+      cons1 `shouldSatisfy` subsetOf w_cons
+      arith0 `shouldSatisfy` subsetOf w_arith
+      arith1 `shouldSatisfy` subsetOf w_arith
+
   where
     nondet = grammar "S" $ M.fromList [ ("S", [ Eps "F" ])
                                       , ("A", [ Ctor "a" [] ])
@@ -332,6 +519,56 @@ spec = do
                                                , ("Type", [ Ctor "Num" []
                                                           , Ctor "Fun" ["Type","Type"]])
                                                , ("String", [ Ctor "String" [] ])]
+
+    cons0 :: GrammarBuilder Text
+    cons0 = grammar "T0" $ M.fromList [ ("T0", [ Ctor "nil" [], Ctor "cons" ["T1","T2"] ])
+                                      , ("T1", [ Ctor "any" [] ])
+                                      , ("T2", [ Ctor "nil" [] ])]
+    cons1 :: GrammarBuilder Text
+    cons1 = grammar "T3" $ M.fromList [ ("T3", [ Ctor "nil" [], Ctor "cons" ["T4","T5"] ])
+                                      , ("T4", [ Ctor "any" [] ])
+                                      , ("T5", [ Ctor "nil" [], Ctor "cons" ["T6","T7"] ])
+                                      , ("T6", [ Ctor "any" [] ])
+                                      , ("T7", [ Ctor "nil" [] ])]
+    cons2 :: GrammarBuilder Text
+    cons2 = grammar "T8" $ M.fromList [ ("T8", [ Ctor "nil" [], Ctor "cons" ["T9","T10"] ])
+                                      , ("T9", [ Ctor "any" [] ])
+                                      , ("T10", [ Ctor "nil" [], Ctor "cons" ["T11","T12"] ])
+                                      , ("T11", [ Ctor "any" [] ])
+                                      , ("T12", [ Ctor "nil" [], Ctor "cons" ["T13","T14"] ])
+                                      , ("T13", [ Ctor "any" [] ])
+                                      , ("T14", [ Ctor "nil" [] ])]
+    cons01 :: GrammarBuilder Text
+    cons01 = grammar "T3" $ M.fromList [ ("T3", [ Ctor "nil" [], Ctor "cons" ["T4", "T3"] ])
+                                       , ("T4", [ Ctor "any" [] ])]
+    cons12 :: GrammarBuilder Text
+    cons12 = grammar "T8" $ M.fromList [ ("T8", [ Ctor "nil" [], Ctor "cons" ["T9", "T10"] ])
+                                       , ("T9", [ Ctor "any" [] ])
+                                       , ("T10", [ Ctor "nil" [], Ctor "cons" ["T11", "T10"] ])
+                                       , ("T11", [ Ctor "any" [] ])]
+
+    arith0 :: GrammarBuilder Text
+    arith0 = grammar "T0" $ M.fromList [ ("T0", [ Ctor "zero" [], Ctor "Add" ["Tx", "T1"] ])
+                                       , ("Tx", [ Ctor "zero" [] ])
+                                       , ("T1", [ Ctor "one" [], Ctor "Mul" ["T1","T2"] ])
+                                       , ("T2", [ Ctor "cst" [], Ctor "par" ["Tx"], Ctor "var" [] ])]
+    arith1 :: GrammarBuilder Text
+    arith1 = grammar "Tn" $ M.fromList [ ("Tn", [ Ctor "zero" [], Ctor "Add" ["T3","T6"] ])
+                                       , ("T3", [ Ctor "zero" [], Ctor "Add" ["Tx","T4"] ])
+                                       , ("Tx", [ Ctor "Zero" [] ])
+                                       , ("T4", [ Ctor "one" [], Ctor "Mul" ["T4","T5"] ])
+                                       , ("T5", [ Ctor "cst" [], Ctor "par" ["Tx"], Ctor "var" [] ])
+                                       , ("T6", [ Ctor "one" [], Ctor "Mul" ["T6","T7"] ])
+                                       , ("T7", [ Ctor "cst" [], Ctor "par" ["T3"], Ctor "var" [] ])]
+    arith2 :: GrammarBuilder Text
+    arith2 = grammar "T0" $ M.fromList [ ("T0", [ Ctor "cst" [], Ctor "var" [] ]) ]
+    arith3 :: GrammarBuilder Text
+    arith3 = grammar "Tn" $ M.fromList [ ("Tn", [ Ctor "cst" [], Ctor "par" ["Tx"], Ctor "var" [] ])
+                                       , ("Tx", [ Ctor "zero" [] ]) ]
+    arith01 :: GrammarBuilder Text
+    arith01 = grammar "Tn" $ M.fromList [ ("Tn", [ Ctor "zero" [], Ctor "Add" ["Tn","T6"] ])
+                                        , ("T6", [ Ctor "one" [], Ctor "Mul" ["T6","T7"] ])
+                                        , ("T7", [ Ctor "var" [], Ctor "par" ["Tn"], Ctor "cst" [] ])]
 
     -- Because equality is implemented using inclusion, we cannot test
     -- these functions by using `shouldBe`, which uses the Eq type
