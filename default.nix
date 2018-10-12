b2d1c80a24562d2b8a873a18d4a4d7112a24f2c5
Sven Keidel <svenkeidel@gmail.com>
2018-10-12 11:07:10 +0200

    update default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, Cabal, containers, deepseq, mtl, parsec
-, random, stdenv, syb
+, random, stdenv, syb, hspec, hashable
 }:
 mkDerivation {
   pname = "tree-automata";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base Cabal containers deepseq mtl parsec random syb
+    base Cabal containers deepseq mtl parsec random syb hspec hashable
   ];
   homepage = "https://github.com/svenkeidel/tree-automata.git";
   description = "Regular tree automata";
