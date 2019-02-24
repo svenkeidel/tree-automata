505e9dd6ae0233a598dc36cfc980f04f35837599
Sven Keidel <svenkeidel@gmail.com>
2019-02-24 16:13:07 +0100

    continue refactoring

--- src/NonTerm.hs
+++ src/NonTerm.hs
@@ -1,34 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module NonTerm(NonTerm,toText,nonTerm,uniqueStart,uniqueNt) where
+{-# LANGUAGE TypeFamilies #-}
+module NonTerm(NonTerm(..)) where
 
-import           Data.Hashable(Hashable)
-import           Data.Text(Text)
-import qualified Data.Text as T
+import Control.Monad.State(State,get,put)
 
-import           Control.Monad.State(State,get,put)
-import           Control.DeepSeq
+class NonTerm n where
+  type Gen n :: *
+  fresh :: State (Gen n) n
 
-newtype NonTerm = NonTerm Text deriving (Eq,Ord,NFData,Hashable)
-
-toText :: NonTerm -> Text
-toText (NonTerm tx) = tx
-
-instance Show NonTerm where show (NonTerm x) = T.unpack x
-
-nonTerm :: Text -> State Int NonTerm
-nonTerm name = do
-  x <- fresh
-  return $ NonTerm $ name `T.append` (T.pack (show x))
-
-uniqueStart :: State Int NonTerm
-uniqueStart = nonTerm "Start"
-
-uniqueNt :: State Int NonTerm
-uniqueNt = nonTerm "Nt"
-
-fresh :: State Int Int
-fresh = do
-  x <- get
-  put (x+1)
-  return x
+instance NonTerm Int where
+  type Gen Int = Int
+  fresh = do
+    x <- get
+    put (x+1)
+    return x
--- src/TreeGrammar.hs
+++ src/TreeGrammar.hs
@@ -3,7 +3,21 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module TreeGrammar where
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module TreeGrammar
+( Grammar
+, GrammarBuilder
+, addConstructor
+, union
+, epsilonClosure
+, dropUnreachable
+, dropUnproductive
+, productive
+, determinize
+, subsetOf
+)
+where
 
 import           Prelude hiding (lookup)
 
@@ -26,17 +40,17 @@ import           NonTerm
 
 type Identifiable a = (Hashable a, Eq a)
 
-data Grammar a = Grammar { start :: NonTerm, productions :: ProdMap a }
-type ProdMap a = HashMap NonTerm (Rhs a)
-data Rhs a = Rhs { constrs :: Constructors a, epsilons :: HashSet NonTerm }
-newtype Constructors a = Constructors (HashMap a (IntMap (HashSet [NonTerm])))
+data Grammar n t = Grammar { start :: n, productions :: ProdMap n t }
+type ProdMap n t = HashMap n (Rhs n t)
+data Rhs n t = Rhs { constrs :: Constructors n t, epsilons :: HashSet n }
+newtype Constructors n t = Constructors (HashMap t (IntMap (HashSet [n])))
 
-type GrammarBuilder a = State Int (Grammar a)
+type GrammarBuilder n t = State (Gen n) (Grammar n t)
 
-mapGrammar :: (Grammar a -> Grammar b) -> GrammarBuilder a -> GrammarBuilder b
-mapGrammar f = fmap f
+-- mapGrammar :: (Grammar n t -> Grammar n t') -> GrammarBuilder n t -> GrammarBuilder n t'
+-- mapGrammar f = fmap f
 
-cons :: Identifiable a => a -> [NonTerm] -> Rhs a
+cons :: (Identifiable n, Identifiable t) => t -> [n] -> Rhs n t
 cons c ts =
   Rhs { constrs = Constructors
                 $ Map.singleton c
@@ -46,23 +60,23 @@ cons c ts =
       }
 
 -- | @L(addConstructor c [g1...gn]) = [ c(t1...tn) | t1 in L(g1) ... tn in L(gn)]@
-addConstructor :: Identifiable a => a -> [GrammarBuilder a] -> GrammarBuilder a
+addConstructor :: (NonTerm n, Identifiable n, Identifiable t) => t -> [GrammarBuilder n t] -> GrammarBuilder n t
 addConstructor n gs = do
-  s <- uniqueStart
+  s <- fresh
   gs' <- sequence gs
   return $ Grammar s $ Map.insert s (cons n (start <$> gs'))
                      $ foldMap productions gs'
 
-union :: Identifiable a => GrammarBuilder a -> GrammarBuilder a -> GrammarBuilder a
+union :: (NonTerm n, Identifiable n, Identifiable t) => GrammarBuilder n t -> GrammarBuilder n t -> GrammarBuilder n t
 union g1 g2 = do
   Grammar start1 prods1 <- g1
   Grammar start2 prods2 <- g2
-  s <- uniqueStart
+  s <- fresh
   return $ Grammar s $ Map.insert s (mempty {epsilons = [start1, start2]})
                      $ Map.unionWith (error "non terminals are not disjoint") prods1 prods2
 
 -- | Inlines all productions reachable via epsilon rules.
-epsilonClosure :: Identifiable a => Grammar a -> Grammar a
+epsilonClosure :: (Identifiable n, Identifiable t) => Grammar n t -> Grammar n t
 epsilonClosure g@(Grammar _ p) = g { productions = Map.mapWithKey close p } 
   where
     close name rhs =
@@ -73,55 +87,55 @@ epsilonClosure g@(Grammar _ p) = g { productions = Map.mapWithKey close p }
           }
 
 -- Computes the set of non-terminals reachable via epsilon rules.
-epsilonReachable :: NonTerm -> ProdMap a -> HashSet NonTerm
+epsilonReachable :: forall n t. Identifiable n => n -> ProdMap n t -> HashSet n
 epsilonReachable n0 p = go n0 Set.empty
   where
-    go :: NonTerm -> HashSet NonTerm -> HashSet NonTerm
+    go :: n -> HashSet n -> HashSet n
     go n r 
       | Set.member n r = r
       | otherwise      = foldl' (flip go) (Set.insert n r) (epsilons (p Map.! n))
 
-lookup :: Identifiable a => NonTerm -> ProdMap a -> Constructors a
+lookup :: (Identifiable n, Identifiable t) => n -> ProdMap n t -> Constructors n t
 lookup n p = constrs $ foldMap (p Map.!) $ Set.toList (epsilonReachable n p)
 
 -- | Removes productions that are not reachable from the start symbol.
-dropUnreachable :: Identifiable a => Grammar a -> Grammar a
+dropUnreachable :: (Identifiable n, Identifiable t) => Grammar n t -> Grammar n t
 dropUnreachable g@(Grammar s p) =
   g { productions = Map.filterWithKey (\k _ -> Set.member k (reachable s g)) p }
 
-reachable :: Identifiable a => NonTerm -> Grammar a -> HashSet NonTerm
+reachable :: forall n t. (Identifiable n, Identifiable t) => n -> Grammar n t -> HashSet n
 reachable n0 (Grammar _ p) = go n0 Set.empty
   where
-    go :: NonTerm -> HashSet NonTerm -> HashSet NonTerm
+    go :: n -> HashSet n -> HashSet n
     go n r
       | Set.member n r = r
       | otherwise      = foldl' (flip go) (Set.insert n r) (nonTerms (lookup n p))
 
-dropUnproductive :: Grammar a -> Grammar a
+dropUnproductive :: Identifiable n => Grammar n t -> Grammar n t
 dropUnproductive g@(Grammar _ p) =
   g { productions = Map.filterWithKey (\k _ -> Set.member k (productive g)) p }
 
 -- | Returns all productive nonterminals in the given grammar.
-productive :: Grammar a -> HashSet NonTerm
+productive :: forall n t. Identifiable n => Grammar n t -> HashSet n
 productive (Grammar _ prods) = go $ Set.fromList [ n | (n, rhs) <- Map.toList prods, producesConstant rhs]
   where
-    producesConstant :: Rhs a -> Bool
+    producesConstant :: Rhs n t -> Bool
     producesConstant rhs = any (any null) (constructors' rhs)
   
-    go :: HashSet NonTerm -> HashSet NonTerm
+    go :: HashSet n -> HashSet n
     go p =
       let p' = Set.union p $ Set.fromList [ n | (n, rhs) <- Map.toList prods, rhsProductive rhs p ]
       in if p == p' then p else go p'
 
     -- A right hand side is productive if one of its epsilon rules is
     -- productive or a all non-terminals of a constructor are productive.
-    rhsProductive :: Rhs a -> HashSet NonTerm -> Bool
+    rhsProductive :: Rhs n t -> HashSet n -> Bool
     rhsProductive rhs prod = any (`Set.member` prod) (epsilons rhs)
                           || any (any (any (all (`Set.member` prod)))) (constructors' rhs)
 
 
-type RenameMap = HashMap (HashSet NonTerm) NonTerm
-determinize :: forall a. Identifiable a => Grammar a -> GrammarBuilder a
+type RenameMap n n' = HashMap (HashSet n) n'
+determinize :: forall n n' t. (NonTerm n', Identifiable n, Identifiable n', Identifiable t) => Grammar n t -> GrammarBuilder n' t
 determinize (Grammar s p) = state $ \i ->
   let (s',(i',(ps,_))) = runState (go [s]) (i,(mempty,mempty))
   in (Grammar s' ps, i')
@@ -133,7 +147,7 @@ determinize (Grammar s p) = state $ \i ->
    --      ...
    --    ]
    -- = [ N1 U N2 U N3 -> foo(A1 U A2 U A3, B1 U B2 U B3) | bar(B1 U B2) | biz(B3) ... ]
-   go :: Identifiable a => HashSet NonTerm -> State (Int, (ProdMap a,RenameMap)) NonTerm
+   go :: HashSet n -> State (Gen n', (ProdMap n' t,RenameMap n n')) n'
    go ns = do
      rmap <- getRmap
      case Map.lookup ns rmap of
@@ -141,62 +155,96 @@ determinize (Grammar s p) = state $ \i ->
        Nothing -> do
          n <- unique
          putRmap $ Map.insert ns n rmap
-         let Constructors prods = foldMap (\k -> lookup k p) (Set.toList ns)
+         let Constructors prods = foldMap (\k -> lookup k p) ns
          ctrs <- traverse (traverse (fmap Set.singleton . traverse go . transpose)) prods
          modifyRes $ Map.insert n (Rhs { constrs = Constructors ctrs, epsilons = mempty})
          return n
 
-   unique :: State (Int,(ProdMap a,RenameMap)) NonTerm
-   unique = state (\(i,x) -> let (f,i') = runState uniqueNt i in (f,(i',x)))
+   unique :: State (Gen n',(ProdMap n' t,RenameMap n n')) n'
+   unique = state (\(i,x) -> let (f,i') = runState fresh i in (f,(i',x)))
 
-   getRmap :: State (Int,(ProdMap a,RenameMap)) RenameMap
+   getRmap :: State (Gen n',(ProdMap n' t',RenameMap n n')) (RenameMap n n')
    getRmap = snd . snd <$> get
 
-   putRmap :: RenameMap -> State (Int,(ProdMap a,RenameMap)) ()
+   putRmap :: RenameMap n n' -> State (Gen n',(ProdMap n' t',RenameMap n n')) ()
    putRmap r = modify (second (second (const r)))
 
-   modifyRes :: (ProdMap a -> ProdMap a) -> State (Int,(ProdMap a,RenameMap)) ()
+   modifyRes :: (ProdMap n' t -> ProdMap n' t) -> State (Gen n',(ProdMap n' t,RenameMap n n')) ()
    modifyRes f = modify (second (first f))
 
-   transpose :: Identifiable x => HashSet [x] -> [HashSet x]
-   transpose = fmap Set.fromList . L.transpose . Set.toList
+transpose :: Identifiable x => HashSet [x] -> [HashSet x]
+transpose = fmap Set.fromList . L.transpose . Set.toList
 
+-- | Test whether the first grammar is a subset of the second,
+-- i.e. whether L(G1) ⊆ L(G2). The return value is either @Nothing@ or
+-- a wittness of which non-terminals are a subset of other
+-- non-terminals.  For example, if @([A,B],[X,Y]) in subsetOf G1 G2@ then
+-- @L(G1(A)) U L(G1(B))  ⊆  L(G2(X)) U L(G2(Y))@.
+subsetOf :: forall n1 n2 t. (Identifiable n1, Identifiable n2, Identifiable t)
+         => Grammar n1 t -> Grammar n2 t -> Maybe [(HashSet n1,HashSet n2)]
+Grammar s1 p1 `subsetOf` Grammar s2 p2 = fmap (fmap (\(x,y) -> (y,x)) . Map.toList) (execStateT (go [s1] [s2]) mempty)
+  -- solve (s1,s2) $ generate Map.empty (Set.singleton (s1,s2))
+  where
+    go :: HashSet n1 -> HashSet n2 -> StateT (HashMap (HashSet n2) (HashSet n1)) Maybe ()
+    go m1 m2 = do
+      seen <- get
+      let xs = Map.lookupDefault mempty m2 seen
+      -- m1 ⊆ xs ⊆ m2
+      unless (m1 `subset` xs) $ do
+        put $ Map.insert m2 (xs `Set.union` m1) seen
+        
+        -- check if (m1-xs) ⊆ m2
+        let m1' = m1 `Set.difference` xs
+            Constructors c1 = foldMap (\k -> lookup k p1) m1'
+            Constructors c2 = foldMap (\k -> lookup k p2) m2
 
-class NonTerminals a where
-  nonTerms :: a -> HashSet NonTerm
+        -- Check if the set of constructors of m1 is a subset of m2.
+        guard $ Map.keysSet c1 `subset` Map.keysSet c2
+        forM_ (Map.intersectionWith (,) c1 c2) $ \(a1,a2) -> do
 
-instance NonTerminals [NonTerm] where
+          -- Check if the set of arities of m1 is a subset of m2.
+          guard $ Set.fromList (IM.keys a1) `subset` Set.fromList (IM.keys a2)
+          forM_ (IM.intersectionWith (,) a1 a2) $ \(l1,l2) ->
+            zipWithM go (transpose l1) (transpose l2)
+
+    subset :: Identifiable a => HashSet a -> HashSet a -> Bool
+    subset m1 m2 = all (`Set.member` m2) m1
+
+class Identifiable n => NonTerminals a n where
+  nonTerms :: a -> HashSet n
+
+instance Identifiable n => NonTerminals [n] n where
   nonTerms = Set.fromList
 
-instance NonTerminals (Grammar a) where
+instance Identifiable n => NonTerminals (Grammar n t) n where
   nonTerms (Grammar _ p) = Set.fromList $ Map.keys p
 
-instance NonTerminals (Rhs a) where
+instance Identifiable n => NonTerminals (Rhs n t) n where
   nonTerms rhs = nonTerms (constrs rhs) <> epsilons rhs
 
-instance NonTerminals n => NonTerminals (HashSet n) where
+instance (Identifiable n, NonTerminals x n) => NonTerminals (HashSet x) n where
   nonTerms rhs = foldMap nonTerms rhs
 
-instance NonTerminals n => NonTerminals (HashMap a n) where
+instance NonTerminals x n => NonTerminals (HashMap a x) n where
   nonTerms m = foldMap nonTerms (Map.elems m)
 
-instance NonTerminals n => NonTerminals (IntMap n) where
+instance NonTerminals x n => NonTerminals (IntMap x) n where
   nonTerms m = foldMap nonTerms (IM.elems m)
 
-instance NonTerminals (Constructors a) where
+instance Identifiable n => NonTerminals (Constructors n t) n where
   nonTerms (Constructors m) = nonTerms m
 
-instance Identifiable a => Semigroup (Constructors a) where
+instance (Identifiable n, Identifiable t) => Semigroup (Constructors n t) where
   Constructors c1 <> Constructors c2 = Constructors (Map.unionWith (IM.unionWith (<>)) c1 c2)
-instance Identifiable a => Monoid (Constructors a) where
+instance (Identifiable n, Identifiable t) => Monoid (Constructors n t) where
   mappend = (<>)
   mempty = Constructors Map.empty
 
-instance Identifiable a => Semigroup (Rhs a) where
+instance (Identifiable n, Identifiable t) => Semigroup (Rhs n t) where
   Rhs c1 e1 <> Rhs c2 e2 = Rhs (c1 <> c2) (e1 <> e2)
-instance Identifiable a => Monoid (Rhs a) where
+instance (Identifiable n, Identifiable t) => Monoid (Rhs n t) where
   mappend = (<>)
   mempty = Rhs { constrs = mempty, epsilons = mempty }
 
-constructors' :: Rhs a -> HashMap a (IntMap (HashSet [NonTerm]))
+constructors' :: Rhs n t -> HashMap t (IntMap (HashSet [n]))
 constructors' (Rhs {constrs = Constructors c}) = c
